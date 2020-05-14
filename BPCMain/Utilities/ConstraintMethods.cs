3a2f515fef6ce8398bfb34bf06b87907ecc22521
JaceDanish <47661433+JaceDanish@users.noreply.github.com>
2020-05-14 15:57:44 +0200

    MailValidation

--- BPCMain/Utilities/ConstraintMethods.cs
+++ BPCMain/Utilities/ConstraintMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,19 @@ namespace BPCMain.Utilities
 {
     static class ConstraintMethods
     {
-        
+        //Mangler at blive testet
+        public static bool IsEmailValid(string eMail)
+        {
+            //MailAddress kaster exceptions hvis addressen ikke er valid, derfor try/catch.
+            try
+            {
+                MailAddress address = new MailAddress(eMail);
+                return address.Address == eMail;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
