b2c275a9adfbe0a1d7c02bc3c340ae33d072651c
Michael Faber Harder <lizvanzanen@gmail.com>
2020-05-15 13:46:59 +0200

    NewUserCar næsten færdig og Constraints påbrgyndt

--- BPCMain/Utilities/ConstraintMethods.cs
+++ BPCMain/Utilities/ConstraintMethods.cs
@@ -4,13 +4,66 @@ using System.Linq;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.SmartCards;
 
 namespace BPCMain.Utilities
 {
     public static class ConstraintMethods
     {
-        //Mangler at blive testet
-        public static bool IsEmailValid(string eMail)
+
+		 public static bool CreateCarCheck(string firstName, string lastName, int cvrNo, string eMail, string telephoneNo, string mobileNo, string address, string postalCode, string country, string password)
+	    {
+			if (StringLengthCheck(firstName, 2, 30) == false ||
+			    StringLengthCheck(lastName, 2, 30) == false ||
+			    CheckNumber(cvrNo, 10000000, 99999999) == false ||
+			    StringLengthCheck(eMail, 10, 30) == false ||
+			    StringLengthCheck(telephoneNo, 8, 8) == false ||
+			    StringLengthCheck(mobileNo, 8, 8) == false ||
+			    StringLengthCheck(address, 5, 30) == false ||
+			    StringLengthCheck(postalCode, 4, 4) == false ||
+			    StringLengthCheck(country, 2, 30) == false ||
+			    StringLengthCheck(password, 6, 16) == false)
+			{
+					return false;
+			}
+			else
+			{
+				return true;
+			}
+	    }
+
+		 public static bool CheckNumber(int number, int min, int max)
+		 {
+			 if (number >= min && number <= max)
+			 {
+				 return true;
+			 }
+			 else
+			 {
+				 return false;
+			 }
+		 }
+
+
+	    public static bool StringLengthCheck(string str, int minLength, int maxLength)
+	    {
+			    if (str.Length >= minLength && str.Length <= maxLength)
+			    {
+				    return true;
+			    }
+			    else
+			    {
+			    return false;
+				 }
+	    }
+
+		 //public static bool OnlyNumbersCheck(string str)
+		 //{
+
+		 //}
+
+		//Mangler at blive testet
+		public static bool IsEmailValid(string eMail)
         {
             //MailAddress kaster exceptions hvis addressen ikke er valid, derfor try/catch.
             try
--- BPCMain/ViewModel/NewUserCar.cs
+++ BPCMain/ViewModel/NewUserCar.cs
@@ -3,26 +3,132 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BPCMain.Utilities;
+using static BPCMain.Utilities.ConstraintMethods;
+using static BPCMain.Utilities.NavigationService;
 
 namespace BPCMain.ViewModel
 {
-    class NewUserCar
+    class NewUserCar : BaseVM
 	{
 		#region Instance Fields
 
+		private string _firstName;
+		private string _lastName;
+		private int _cvrNo;
+		private string _eMail;
+		private string _telephoneNo;
+		private string _mobileNo;
+		private string _address;
+		private string _postalCode;
+		private string _country;
+		private string _password;
 
+		private string _errorMessage;
+
+		private RelayCommand _createCar;
+		
+		private NavigationService navigation = new NavigationService();
 
 		#endregion
 
 		#region Properties
 
+		public string FirstName
+		{
+			get { return _firstName; }
+			set
+			{
+				_firstName = value;
+			}
+		}
 
+		public string LastName
+		{
+			get { return _lastName; }
+			set { _lastName = value; }
+		}
+
+		public int CvrNo
+		{
+			get { return _cvrNo; }
+			set { _cvrNo = value; }
+		}
+
+		public string EMail
+		{
+			get { return _eMail; }
+			set { _eMail = value; }
+		}
+
+		public string TelephoneNo
+		{
+			get { return _telephoneNo; }
+			set { _telephoneNo = value; }
+		}
+
+		public string MobileNo
+		{
+			get { return _mobileNo; }
+			set { _mobileNo = value; }
+		}
+
+		public string Address
+		{
+			get { return _address; }
+			set { _address = value; }
+		}
+
+		public string PostalCode
+		{
+			get { return _postalCode; }
+			set { _postalCode = value; }
+		}
+
+		public string Country
+		{
+			get { return _country; }
+			set { _country = value; }
+		}
+
+		public string Password
+		{
+			get { return _password; }
+			set { _password = value; }
+		}
+
+		public string ErrorMessage
+		{
+			get { return _errorMessage; }
+			set
+			{
+				_errorMessage = value;
+				OnPropertyChanged();
+			}
+		}
 
 		#endregion
 
 		#region RelayCommands
 
-
+		public RelayCommand CreateCar
+		{
+			get { return _createCar; }
+			set
+			{
+				if (CreateCarCheck(FirstName, LastName, CvrNo, EMail, TelephoneNo, MobileNo, Address, PostalCode,
+						Country, Password) == true)
+				{
+					//save new Car in database
+					
+					navigation.Navigate(typeof(BPCMain.View.DisplayBookingCar));
+				}
+				else
+				{
+					ErrorMessage = "Fejl i oplysninger";
+				}
+			}
+		}
 
 		#endregion
 	}
