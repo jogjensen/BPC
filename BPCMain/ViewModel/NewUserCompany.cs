6d763c25732e76e0c4740196c9f9d006499d3557
Philip Lind Jensen <jogjensen@hotmail.com>
2020-05-17 16:38:33 +0200

    Started NewUserCompany VM

--- BPCMain/ViewModel/NewUserCompany.cs
+++ BPCMain/ViewModel/NewUserCompany.cs
@@ -1,29 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using BPCMain.Utilities;
+using BPCClassLibrary;
 
 namespace BPCMain.ViewModel
 {
-    class NewUserCompany
+    class NewUserCompany : BaseVM
 	{
-		#region Instance Fields
+        #region Instance Fields
+        private string _companyName;
+        private int _cvrNo;
+        private string _eMail;
+        private string _telephoneNo;
+        private string _mobileNo;
+        private string _address;
+        private string _postalCode;
+        private string _country;
+        private string _password;
+        private int _truckdriver;
+        private RelayCommand _createCompany;
+        #endregion
 
+        #region Properties
 
+        public string CompanyName
+        {
+            get { return _companyName; }
+            set { _companyName = value; }
+        }
 
-		#endregion
+        public int CvrNo
+        {
+            get { return _cvrNo; }
+            set
+            { _cvrNo = value; }
+        }
 
-		#region Properties
+        public string EMail
+        {
+            get { return _eMail; }
+            set
+            { _eMail = value; }
+        }
 
+        public string TelephoneNo
+        {
+            get { return _telephoneNo; }
+            set
+            { _telephoneNo = value; }
+        }
 
+        public string MobileNo
+        {
+            get { return _mobileNo; }
+            set { _mobileNo = value; }
+        }
 
-		#endregion
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
 
-		#region RelayCommands
+        public string PostalCode
+        {
+            get { return _postalCode; }
+            set { _postalCode = value; }
+        }
 
+        public string Country
+        {
+            get { return _country; }
+            set { _country = value; }
+        }
 
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
 
-		#endregion
-	}
+        public int Truckdriver
+        {
+            get { return _truckdriver; }
+            set { _truckdriver = value; }
+        }
+
+        #endregion
+
+        #region RelayCommands
+
+        //public RelayCommand CreateCompany
+        //{
+        //    get { return _createCompany; }
+        //    set
+        //    {
+        //        string errorMessage =
+        //    }
+        //}
+
+        #endregion
+    }
 }
