f1210dc67a15bd5f63b548158df4afe1671f7993
JaceDanish <47661433+JaceDanish@users.noreply.github.com>
2020-05-19 09:53:55 +0200

    Update CarBooking.cs

--- BPCClassLibrary/CarBooking.cs
+++ BPCClassLibrary/CarBooking.cs
@@ -4,26 +4,40 @@ using System.Text;
 
 namespace BPCClassLibrary
 {
-    class CarBooking
+    public class CarBooking
     {
         private int _orderNo;
         private int _car;
+        private int _id;
 
-        public CarBooking(int orderNo)
+        public CarBooking(int orderNo, int CBId)
         {
             _orderNo = orderNo;
             _car = 0;
+            _id = CBId;
+        }
+
+        public CarBooking()
+        {
+
         }
 
         public int OrderNo
         {
             get => _orderNo;
+            set => _orderNo = value;
         }
 
         public int Car
         {
             get => _car;
             set => _car = value;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
         }
     }
 }
