dae9e10104e78852f3aefd36c32a50f4e38a9615
JaceDanish <47661433+JaceDanish@users.noreply.github.com>
2020-05-19 09:38:20 +0200

    CarBooking

---
+++ BPCClassLibrary/CarBooking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BPCClassLibrary
+{
+    class CarBooking
+    {
+        private int _orderNo;
+        private int _car;
+
+        public CarBooking(int orderNo)
+        {
+            _orderNo = orderNo;
+            _car = 0;
+        }
+
+        public int OrderNo
+        {
+            get => _orderNo;
+        }
+
+        public int Car
+        {
+            get => _car;
+            set => _car = value;
+        }
+    }
+}
