bdb6aafa371f44486f6668fe3fdfe401973f6dbc
Michael Faber Harder <lizvanzanen@gmail.com>
2020-06-15 09:50:00 +0200

    mandag inden eksamen

--- BPCMain/Utilities/ConstraintMethods.cs
+++ BPCMain/Utilities/ConstraintMethods.cs
@@ -38,26 +38,25 @@ namespace BPCMain.Utilities
 
 		public static bool CreateUserCheck(Customer newCustomer)
 		{
-			if (
-				!StringLengthCheck(newCustomer.CompanyName, 2,50)||
-				!CheckNumber(newCustomer.CvrNo, 10000000,99999999)||
-				!StringLengthCheck(newCustomer.EMail,3,50)||
-				!StringLengthCheck(newCustomer.TelephoneNo,8,8)||
-				!OnlyNumbersCheck(newCustomer.TelephoneNo) ||
-				!StringLengthCheck(newCustomer.MobileNo,0,16)||
-				!OnlyNumbersCheck(newCustomer.MobileNo)||
-				!StringLengthCheck(newCustomer.Address,6,80)||
-				!StringLengthCheck(newCustomer.PostalCode,4,10)||
-				!OnlyNumbersCheck(newCustomer.PostalCode)||
-				!StringLengthCheck(newCustomer.Country,2,40)||
-				!StringLengthCheck(newCustomer.Password,6,30)) return false;
+			//if (
+			//	!StringLengthCheck(newCustomer.CompanyName, 2,50)||
+			//	!CheckNumber(newCustomer.CvrNo, 10000000,99999999)||
+			//	!StringLengthCheck(newCustomer.EMail,3,50)||
+			//	!StringLengthCheck(newCustomer.TelephoneNo,8,8)||
+			//	!OnlyNumbersCheck(newCustomer.TelephoneNo) ||
+			//	!StringLengthCheck(newCustomer.MobileNo,0,16)||
+			//	!OnlyNumbersCheck(newCustomer.MobileNo)||
+			//	!StringLengthCheck(newCustomer.Address,6,80)||
+			//	!StringLengthCheck(newCustomer.PostalCode,4,10)||
+			//	!OnlyNumbersCheck(newCustomer.PostalCode)||
+			//	!StringLengthCheck(newCustomer.Country,2,40)||
+			//	!StringLengthCheck(newCustomer.Password,6,30)) return false;
 			return true;
 		}
 
 		public static bool CreateBookingCheck(Booking booking) 
 		{
 			if (
-				!CheckNumber(booking.CompanyCvrNo, 10000000, 99999999) ||
 				!CheckNumber(booking.NumOfCarsNeeded, 1, 4)) return false;
 			return true;
 		}
--- BPCMain/ViewModel/NewUserCompany.cs
+++ BPCMain/ViewModel/NewUserCompany.cs
@@ -12,186 +12,186 @@ using BPCMain.View;
 
 namespace BPCMain.ViewModel
 {
-    class NewUserCompany : BaseVM
+	class NewUserCompany : BaseVM
 	{
-        #region Instance Fields
-        private string _companyName;
-        private int _cvrNo;
-        private string _eMail;
-        private string _telephoneNo;
-        private string _mobileNo;
-        private string _address;
-        private string _postalCode;
-        private string _city;
-        private string _country;
-        private string _password;
-        private int _truckdriverId;
-        private RelayCommand _createCompany;
-        private RelayCommand _backCommand;
-        private RelayCommand _faqCommand;
-        private RelayCommand _aboutBpcCommand;
-        private RelayCommand _contactBpcCommand;
-        private string _errorMessage;
-        private NavigationService navigation = new NavigationService();
-        private RestWorker restworker = new RestWorker();
-        #endregion
+		#region Instance Fields
+		private string _companyName;
+		private int _cvrNo;
+		private string _eMail;
+		private string _telephoneNo;
+		private string _mobileNo;
+		private string _address;
+		private string _postalCode;
+		private string _city;
+		private string _country;
+		private string _password;
+		private int _truckdriverId;
+		private RelayCommand _createCompany;
+		private RelayCommand _backCommand;
+		private RelayCommand _faqCommand;
+		private RelayCommand _aboutBpcCommand;
+		private RelayCommand _contactBpcCommand;
+		private string _errorMessage;
+		private NavigationService navigation = new NavigationService();
+		private RestWorker restworker = new RestWorker();
+		#endregion
 
-        #region Properties
+		#region Properties
 
-        public string CompanyName
-        {
-            get { return _companyName; }
-            set { _companyName = value; }
-        }
+		public string CompanyName
+		{
+			get { return _companyName; }
+			set { _companyName = value; }
+		}
 
-        public int CvrNo
-        {
-            get { return _cvrNo; }
-            set
-            { _cvrNo = value; }
-        }
+		public int CvrNo
+		{
+			get { return _cvrNo; }
+			set
+			{ _cvrNo = value; }
+		}
 
-        public string EMail
-        {
-            get { return _eMail; }
-            set
-            { _eMail = value; }
-        }
+		public string EMail
+		{
+			get { return _eMail; }
+			set
+			{ _eMail = value; }
+		}
 
-        public string TelephoneNo
-        {
-            get { return _telephoneNo; }
-            set
-            { _telephoneNo = value; }
-        }
+		public string TelephoneNo
+		{
+			get { return _telephoneNo; }
+			set
+			{ _telephoneNo = value; }
+		}
 
-        public string MobileNo
-        {
-            get { return _mobileNo; }
-            set { _mobileNo = value; }
-        }
+		public string MobileNo
+		{
+			get { return _mobileNo; }
+			set { _mobileNo = value; }
+		}
 
-        public string Address
-        {
-            get { return _address; }
-            set { _address = value; }
-        }
+		public string Address
+		{
+			get { return _address; }
+			set { _address = value; }
+		}
 
-        public string PostalCode
-        {
-            get { return _postalCode; }
-            set { _postalCode = value; }
-        }
+		public string PostalCode
+		{
+			get { return _postalCode; }
+			set { _postalCode = value; }
+		}
 
-        public string City
-        {
-	        get { return _city; }
-	        set { _city = value; }
-        }
+		public string City
+		{
+			get { return _city; }
+			set { _city = value; }
+		}
 
-        public string Country
-        {
-            get { return _country; }
-            set { _country = value; }
-        }
+		public string Country
+		{
+			get { return _country; }
+			set { _country = value; }
+		}
 
-        public string Password
-        {
-            get { return _password; }
-            set { _password = value; }
-        }
+		public string Password
+		{
+			get { return _password; }
+			set { _password = value; }
+		}
 
-        public int TruckdriverId
-        {
-            get { return _truckdriverId; }
-            set { _truckdriverId = value; }
-        }
+		public int TruckdriverId
+		{
+			get { return _truckdriverId; }
+			set { _truckdriverId = value; }
+		}
 
-        public string ErrorMessage
-        {
-            get { return _errorMessage; }
-            set { _errorMessage = value; OnPropertyChanged(); }
-        }
+		public string ErrorMessage
+		{
+			get { return _errorMessage; }
+			set { _errorMessage = value; OnPropertyChanged(); }
+		}
 
-        public RelayCommand CreateCompany
-        {
-            get { return _createCompany; }
-        }
+		public RelayCommand CreateCompany
+		{
+			get { return _createCompany; }
+		}
 
-        public RelayCommand BackCommand
-        {
-            get { return _backCommand; }
-        }
-        public RelayCommand FAQCommand
-        {
-            get { return _faqCommand; }
-        }
+		public RelayCommand BackCommand
+		{
+			get { return _backCommand; }
+		}
+		public RelayCommand FAQCommand
+		{
+			get { return _faqCommand; }
+		}
 
-        public RelayCommand AboutBpcCommand
-        {
-            get { return _aboutBpcCommand; }
-        }
+		public RelayCommand AboutBpcCommand
+		{
+			get { return _aboutBpcCommand; }
+		}
 
-        public RelayCommand ContactBpcCommand
-        {
-            get { return _contactBpcCommand; }
-        }
-        #endregion
+		public RelayCommand ContactBpcCommand
+		{
+			get { return _contactBpcCommand; }
+		}
+		#endregion
 
-        #region RelayCommands
+		#region RelayCommands
 
-        public NewUserCompany()
-        {
-	        _createCompany = new RelayCommand(NewUser, null);
-            _backCommand = new RelayCommand(GoBack, null);
-            _faqCommand = new RelayCommand(NavigateToFaq, null);
-            _aboutBpcCommand = new RelayCommand(NavigateToAboutBpc, null);
-            _contactBpcCommand = new RelayCommand(NavigateToContactBpc, null);
-        }
-        
-        public async void NewUser()
-        {
-            Customer newCustomer = new Customer(CompanyName, CvrNo, 0, EMail, TelephoneNo, Address, PostalCode, City, Country, Password, MobileNo);
-	        if (ConstraintMethods.CreateUserCheck(newCustomer))
-	        {
-		        await CreateNewCustomer(newCustomer);
-		        navigation.Navigate(typeof(BPCMain.View.DisplayBookingCompany));
-	        }
-	        else
-	        {
-		        ErrorMessage = "Fejl i oplysninger";
-		        navigation.Navigate(typeof(BPCMain.View.HomePageLogin));
-            }
+		public NewUserCompany()
+		{
+			_createCompany = new RelayCommand(NewUser, null);
+			_backCommand = new RelayCommand(GoBack, null);
+			_faqCommand = new RelayCommand(NavigateToFaq, null);
+			_aboutBpcCommand = new RelayCommand(NavigateToAboutBpc, null);
+			_contactBpcCommand = new RelayCommand(NavigateToContactBpc, null);
+		}
+		
+		public async void NewUser()
+		{
+			Customer newCustomer = new Customer(CompanyName, CvrNo, 0, EMail, TelephoneNo, Address, PostalCode, City, Country, Password, MobileNo);
+			if (ConstraintMethods.CreateUserCheck(newCustomer))
+			{
+				await CreateNewCustomer(newCustomer);
+				navigation.Navigate(typeof(BPCMain.View.DisplayBookingCompany));
+			}
+			else
+			{
+				ErrorMessage = "Fejl i oplysninger";
+				navigation.Navigate(typeof(BPCMain.View.HomePageLogin));
+			}
 
-        }
-        public async Task<bool> CreateNewCustomer(Customer newCustomer)
-        {
-	        bool created = await restworker.CreateObjectAsync<Customer>(newCustomer, Datastructures.TableName.Customer);
-	        return created;
-        }
-        #endregion
+		}
+		public async Task<bool> CreateNewCustomer(Customer newCustomer)
+		{
+			bool created = await restworker.CreateObjectAsync<Customer>(newCustomer, Datastructures.TableName.Customer);
+			return created;
+		}
+		#endregion
 
-        #region Methods
+		#region Methods
 
-        public void GoBack()
-        {
-            navigation.GoBack();
-        }
+		public void GoBack()
+		{
+			navigation.GoBack();
+		}
 
-        private void NavigateToContactBpc()
-        {
-            navigation.Navigate(typeof(ContactBPC));
-        }
+		private void NavigateToContactBpc()
+		{
+			navigation.Navigate(typeof(ContactBPC));
+		}
 
-        private void NavigateToFaq()
-        {
-            navigation.Navigate(typeof(Faq));
-        }
+		private void NavigateToFaq()
+		{
+			navigation.Navigate(typeof(Faq));
+		}
 
-        private void NavigateToAboutBpc()
-        {
-            navigation.Navigate(typeof(AboutUs));
-        }
-        #endregion
-    }
+		private void NavigateToAboutBpc()
+		{
+			navigation.Navigate(typeof(AboutUs));
+		}
+		#endregion
+	}
 }
